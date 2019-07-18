6af416ec9cae4bdeb4cb23c8e995d69099ce0d2a
Andres Felipe Mape <af.mape@uniandes.edu.co>
2019-07-18 14:48:26 -0500

    Estilo de la tarjeta

--- MyCode/ClientApp/src/app/app.module.ts
+++ MyCode/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TweetModule } from './tweet/tweet.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TweetModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MyCode/ClientApp/src/app/tweet/tweet.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [ListComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ListComponent
+  ]
+})
+export class TweetModule { }
