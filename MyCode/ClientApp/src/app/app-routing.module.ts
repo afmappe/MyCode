a6118ba821eb0bff979ed26ee19a6e5fca49e277
Andres Felipe Mape <af.mape@uniandes.edu.co>
2019-07-19 13:47:44 -0500

    Ajuste estilos

--- MyCode/ClientApp/src/app/app-routing.module.ts
+++ MyCode/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListComponent } from './tweet/list/list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tweets',
+    pathMatch: 'full'
+  },
+  { path: 'tweets', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MyCode/ClientApp/src/app/tweet/list/list.component.ts
+++ MyCode/ClientApp/src/app/tweet/list/list.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
-})
-export class ListComponent implements OnInit {
+@Component( {
+    selector: 'app-list',
+        templateUrl: './list.component.html',
+        styleUrls: ['./list.component.scss']
+}
 
-  showDetail: boolean = false;
+) export class ListComponent implements OnInit {
 
-  constructor() { }
+    showDetail: boolean=false;
+    tweets: number[]=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-  ngOnInit() {
-  }
+    constructor() {}
 
-  onShowDetail(): void {
-    this.showDetail = !this.showDetail;
-  }
+    ngOnInit() {}
+
+    onShowDetail(): void {
+        this.showDetail= !this.showDetail;
+    }
 
 }
